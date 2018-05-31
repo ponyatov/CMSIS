10cf1395d312419f107063e1b950b939416c1b1e
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-05-31 16:16:41 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- src/f0disco_demo.c
+++ src/f0disco_demo.c
@@ -1,3 +1,17 @@
+/** @file
+ * @brief `f0discovery` demo program
+
+ - @ref STM32F051R8 microcontroller featuring
+ - 64 KB Flash memory, 8 KB RAM
+ - in an LQFP64 package
+
+ * */
+
+#include "stm32f0_discovery.h"
+#include "stm32f0xx.h"
+#include "stm32f0xx_gpio.h"
+#include "stm32f0xx_rcc.h"
+
 void main(void) {
 	for(;;);
 }
